abb045c6ea2d0a9c3159e4242424bcfc4aa3f876
Luke Lampman <lukeb.lampman@gmail.com>
2021-05-12 13:19:53 -0500

    add render function with containing SearchResults class

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APIKey = ""; // Insert your API Key here
+let APIKey = ""; // Insert your API Key here
 const defaultURL = "http://www.omdbapi.com/?s=";
 const getMovies = async(searchTerm) => {
   const response = await fetch(`${defaultURL}${searchTerm}&apikey=${APIKey}`);
@@ -10,5 +10,24 @@ const getMovies = async(searchTerm) => {
   return data;
 }
 getMovies('Star+Wars')
-  .then((data) => {console.log(`Resolved`, data)})
+  .then((data) => {
+    const searchResults = new SearchResults(data.Search);
+    searchResults.render();
+  })
   .catch((err) => console.log(`failed: `, err.message));
+// x.Search[0].Poster
+  class SearchResults{
+    constructor(arrayResults){
+      this.results = arrayResults;
+    }
+    render(){
+      const resultsList = document.querySelector(".movies");
+      const resultsArray = document.querySelectorAll(".card"); 
+      for(let movie of resultsArray){
+        movie.remove();
+      }
+      for(let movie of this.results){
+        resultsList.insertAdjacentHTML('beforeend', `<img src="${movie.Poster}" class="card" alt="${movie.Title} poster">`)
+      }
+    }
+  }
