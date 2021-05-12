72880ebd41404d98e7295250306027dcad568b3f
Luke Lampman <lukeb.lampman@gmail.com>
2021-05-12 13:57:34 -0500

    add working eventListener for #titleSearch

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let APIKey = ""; // Insert your API Key here
+// my API KEY IS 458c2882
+let APIKey = "458c2882"; // Insert your API Key here
 const defaultURL = "http://www.omdbapi.com/?s=";
 const getMovies = async(searchTerm) => {
   const response = await fetch(`${defaultURL}${searchTerm}&apikey=${APIKey}`);
@@ -9,25 +10,47 @@ const getMovies = async(searchTerm) => {
   }
   return data;
 }
-getMovies('Star+Wars')
-  .then((data) => {
-    const searchResults = new SearchResults(data.Search);
-    searchResults.render();
-  })
-  .catch((err) => console.log(`failed: `, err.message));
-// x.Search[0].Poster
-  class SearchResults{
-    constructor(arrayResults){
-      this.results = arrayResults;
+
+class SearchResults{
+  constructor(arrayResults){
+    this.results = arrayResults;
+  }
+  render(){
+    const resultsList = document.querySelector(".movies");
+    const resultsArray = document.querySelectorAll(".card"); 
+    for(let movie of resultsArray){
+      movie.remove();
     }
-    render(){
-      const resultsList = document.querySelector(".movies");
-      const resultsArray = document.querySelectorAll(".card"); 
-      for(let movie of resultsArray){
-        movie.remove();
-      }
-      for(let movie of this.results){
-        resultsList.insertAdjacentHTML('beforeend', `<img src="${movie.Poster}" class="card" alt="${movie.Title} poster">`)
-      }
+    for(let movie of this.results){
+      resultsList.insertAdjacentHTML('beforeend', `<img src="${movie.Poster}" class="card" alt="${movie.Title} poster" title="${movie.Title}">`)
     }
   }
+}
+
+document.querySelector("#titleSearch").addEventListener("keydown", function(e){
+  if(e.code === 'Enter'){
+    e.preventDefault();
+    let searchValue = e.originalTarget.value;
+    searchValue = sanitize(searchValue);
+    getMovies(searchValue)
+.then((data) => {
+  const searchResults = new SearchResults(data.Search);
+  searchResults.render();
+})
+.catch((err) => console.log(`failed: `, err.message));
+  }
+});
+
+function sanitize(input){
+  // replace spaces
+  return input.replace('/\s/g', "+");
+}
+
+
+// getMovies('Star+Wars')
+// .then((data) => {
+//   const searchResults = new SearchResults(data.Search);
+//   searchResults.render();
+// })
+// .catch((err) => console.log(`failed: `, err.message));
+// x.Search[0].Poster
